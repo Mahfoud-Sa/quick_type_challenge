d35c019809fd591607de17bcdd08ddcae28aabea
Mahfoud-Sa <binsabbah2013@gmail.com>
2024-01-02 09:39:07 +0300

    init commit

--- javascript.js
+++ javascript.js
@@ -1,5 +1,14 @@
-
+counter=0;
+errorCounter=0;
 function myfunction(){
-    
+    var currentChar=document.getElementById('text').innerHTML.charAt(counter);
+    var userInput=document.getElementById('value').value.charAt(counter);
+   // alert(currentChar);
+   // alert(userInput);
+    if(currentChar!==userInput){
+        errorCounter+=1;
+    }
+    counter+=1;
+    document.getElementById('score').innerHTML=errorCounter;
     document.getElementById('Demo').innerHTML=document.getElementById('value').value;
 }

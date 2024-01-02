c9012a5b7a94b3a1b7c3bd255a776506d0ccfb4c
Mahfoud-Sa <binsabbah2013@gmail.com>
2024-01-02 20:51:18 +0300

    fix errors

--- javascript.js
+++ javascript.js
@@ -1,14 +1,40 @@
 counter=0;
 errorCounter=0;
 function myfunction(){
-    var currentChar=document.getElementById('text').innerHTML.charAt(counter);
-    var userInput=document.getElementById('value').value.charAt(counter);
+    var text=document.getElementById('text').innerHTML;
+    var currentChar=text.charAt(counter);
+    var userInput=document.getElementById('value').value.slice(-1);
+    var helpText=document.getElementById('Demo').innerHTML=document.getElementById('Demo').innerHTML+currentChar;
+   //alert(currentChar);
+   //alert(userInput);
+   // alert(text);
+    if(counter==text.length){
+
+        alert((text.length/5)/count);
+        location.reload()
+       // document.ref
+    }
+    
    // alert(currentChar);
    // alert(userInput);
     if(currentChar!==userInput){
         errorCounter+=1;
+        var snd = new Audio('/assets/error_sound.wav');
+        snd.play();
+    }else{
+        counter+=1;
     }
-    counter+=1;
+    
     document.getElementById('score').innerHTML=errorCounter;
-    document.getElementById('Demo').innerHTML=document.getElementById('value').value;
+    
 }
+count=0;
+const timer = setInterval(function() {
+  count++;
+  document.getElementById('timer').innerHTML=count;
+  console.log(count);
+  if (count === 0) {
+    clearInterval(timer);
+    console.log("Time's up!");
+  }
+}, 1000);
